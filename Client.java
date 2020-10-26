ad3e3789ea893ddbaf13b2f39a1e71213d0fdd5a
18065791PhungThiHang <72064431+18065791PhungThiHang@users.noreply.github.com>
2020-10-26 19:15:09 +0700

    Bai 1: Doc so

--- Client.java
+++ Client.java
@@ -1,4 +1,4 @@
-package chat;
+package doc;
 
 import java.io.IOException;
 import java.io.PrintStream;
@@ -7,33 +7,20 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
-	private static Scanner inFromServer;
-	private static Scanner scan;
-
 	public static void main(String[] args) {
 		try {
 		@SuppressWarnings("resource")
 		Socket client = new Socket("LocalHost", 9540);
 		System.out.println("Client da duoc tao");
-		inFromServer = new Scanner(client.getInputStream());
+		Scanner inFromServer = new Scanner(client.getInputStream());
 		PrintStream outToServer = new PrintStream(client.getOutputStream());
 		System.out.println("server: " + inFromServer.nextLine());
-		while(true) {
-			System.out.print(">>>");
-			scan = new Scanner(System.in);
-			String text = scan.nextLine();
-			outToServer.println(text);
-			String txt = inFromServer.nextLine();
-			System.out.println("server: " + txt);
-			if(text.equals("exit")) {
-				break;
-			}
-			if(txt.equals("exit")) {
-				System.out.println("server: " + inFromServer.nextLine());
-				break;
-			}		
-			}
-		
+		System.out.print(">>> ");
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		String ten = scan.nextLine();
+		outToServer.println(ten);
+		System.out.println("server: " + inFromServer.nextLine());
 		} catch (UnknownHostException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
@@ -44,3 +31,4 @@ public class Client {
 		}
 
 }
+
