6da425f62752fc440aaed756247a3ffe2bccb9e7
18065791PhungThiHang <72064431+18065791PhungThiHang@users.noreply.github.com>
2020-11-01 23:22:43 +0700

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,29 @@
+package tuan5;
+
+import java.net.MalformedURLException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
+
+public class Server {
+    public static void main(String args[]) {
+
+        try {
+            LocateRegistry.createRegistry(2007);
+            
+            // Sign in rmiregistry
+            IFlight x = new FlightImpl();
+            Naming.bind("rmi://localhost:2007/in4RMI", x );
+            System.out.println("<<<< SERVER ACTIVATED >>>>");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (AlreadyBoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
